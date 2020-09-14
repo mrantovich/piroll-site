2d4e70a97c0eda444d820b1d40eaaf31fb353baf
Mikhail Rantovich <mrantovich@gmail.com>
2020-09-14 22:21:56 +0700

    Секция с видео + адаптив + добавление работ через JQ

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 
+	var moreWorks = '<div class="works__box"><div class="works__item"><img src="img/works/work-04.jpg" alt=""></div> <div class="works__item"><img src="img/works/work-05.jpg" alt=""></div> <div class="works__item"><img src="img/works/work-03.jpg" alt=""></div> <div class="works__item"><img src="img/works/work-01.jpg" alt=""></div> <div class="works__item"><img src="img/works/work-02.jpg" alt=""></div> <div class="works__item"><img src="img/works/work-07.jpg" alt=""></div> <div class="works__item"><img src="img/works/work-08.jpg" alt=""></div> <div class="works__item"><img src="img/works/work-06.jpg" alt=""></div> </div>';
+
 	$('.burger').on('click', function() {
 		$(this).toggleClass('_active');
+	});
+	$('.works__more').on('click', function() {
+		$('.works').append(moreWorks);
+		$(this).remove();
 	});
 
 });
