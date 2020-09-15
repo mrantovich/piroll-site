ba578704d12a490483c59250d5cd99bfe0eb54bb
Mikhail Rantovich <mrantovich@gmail.com>
2020-09-15 18:53:15 +0700

    Сделан текстовый слайдер

--- js/script.js
+++ js/script.js
@@ -5,9 +5,46 @@ $(document).ready(function(){
 	$('.burger').on('click', function() {
 		$(this).toggleClass('_active');
 	});
+
 	$('.works__more').on('click', function() {
 		$('.works').append(moreWorks);
 		$(this).remove();
 	});
+
+	$('.text-slider__dot').on('click', function(){
+		if (!$(this).hasClass('text-slider__dot_active')) {
+			$('.text-slider__dot').removeClass('text-slider__dot_active');
+			$(this).toggleClass('text-slider__dot_active');
+			if ($(this).hasClass('text-slider__dot_first')) {
+				$('.text-slider__items').removeClass('second-is-active third-is-active').addClass('first-is-active');
+			}
+			else if ($(this).hasClass('text-slider__dot_second')) {
+				$('.text-slider__items').removeClass('first-is-active third-is-active').addClass('second-is-active');
+			}
+			else if ($(this).hasClass('text-slider__dot_third')) {
+				$('.text-slider__items').removeClass('first-is-active second-is-active').addClass('third-is-active');
+			}
+		};
+	});
+
+	var intv;
+	var currentDot = 1;
+	var dotsLength = $('.text-slider__dot').length;
+
+	setInterval(function() {
+		if ($('.text-slider__dot_first').hasClass('text-slider__dot_active')) {
+			$(this).remove('text-slider__dot_active');
+			$('.text-slider__dot_second').click();
+		}
+		else if ($('.text-slider__dot_second').hasClass('text-slider__dot_active')) {
+			$(this).remove('text-slider__dot_active');
+			$('.text-slider__dot_third').click();
+		}
+		else if ($('.text-slider__dot_third').hasClass('text-slider__dot_active')) {
+			$(this).remove('text-slider__dot_active');
+			$('.text-slider__dot_first').click();
+		}
+	}, 5000);
+
 
 });
